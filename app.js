3eaa0c78ea82f080a6883fd73dc90bcbbb8dab78
Mikhail Zakharov <62329450+Misha-Mad@users.noreply.github.com>
2020-10-17 16:16:10 +0300

    create middleware for auth

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose');
 const usersRouter = require('./routes/users');
 const cardsRouter = require('./routes/cards');
 const { login, createUser } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -21,15 +22,10 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 
 app.use(limiter);
-app.use((req, res, next) => {
-  req.user = {
-    _id: '5f671ccd51298438e092f2ef',
-  };
-  next();
-});
 app.use(express.json());
 app.post('/signin', login);
 app.post('/signup', createUser);
+app.use(auth);
 app.use(usersRouter);
 app.use(cardsRouter);
 app.get('/*', (req, res) => {
