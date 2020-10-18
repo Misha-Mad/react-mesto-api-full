115e3fa6675d9d20d749f3e83ae99ca284f20f3d
Mikhail Zakharov <62329450+Misha-Mad@users.noreply.github.com>
2020-10-18 20:19:05 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -63,7 +63,7 @@ module.exports.login = (req, res) => {
     .then((user) => {
       // eslint-disable-next-line no-undef
       const token = jwt.sign({ _id: user._id }, process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
-      res.send({ token });
+      res.send(token);
     })
     .catch((err) => {
       res
