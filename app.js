8c67ab35da67476164f575c4c2280791e4d0764a
Mikhail Zakharov <62329450+Misha-Mad@users.noreply.github.com>
2020-10-18 17:32:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.listen(PORT);
 
 app.use(cors());
-app.use(limiter);
+// app.use(limiter);
 app.use(express.json());
 
 app.post('/signin', login);
