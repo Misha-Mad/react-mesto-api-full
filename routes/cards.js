9b83e35e08ad1a8459526bf49a73a4b60b72082a
Mikhail Zakharov <62329450+Misha-Mad@users.noreply.github.com>
2020-10-18 15:27:12 +0300

    disasbled auth middleware

--- routes/cards.js
+++ routes/cards.js
@@ -1,12 +1,12 @@
 const cardsRouter = require('express').Router();
-const auth = require('../middlewares/auth');
+// const auth = require('../middlewares/auth');
 const {
   getCards, createCard, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
-cardsRouter.get('/cards', auth, getCards);
-cardsRouter.post('/cards', auth, createCard);
-cardsRouter.delete('/cards/:cardId', auth, deleteCard);
-cardsRouter.put('/cards/:cardId/likes', auth, likeCard);
-cardsRouter.delete('/cards/:cardId/likes', auth, dislikeCard);
+cardsRouter.get('/cards', getCards);
+cardsRouter.post('/cards', createCard);
+cardsRouter.delete('/cards/:cardId', deleteCard);
+cardsRouter.put('/cards/:cardId/likes', likeCard);
+cardsRouter.delete('/cards/:cardId/likes', dislikeCard);
 module.exports = cardsRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 const usersRouter = require('express').Router();
-const auth = require('../middlewares/auth');
+// const auth = require('../middlewares/auth');
 const {
   getUsers, getUserById, updateUser, updateAvatar,
 } = require('../controllers/users');
 
-usersRouter.get('/users', auth, getUsers);
-usersRouter.get('/users/:id', auth, getUserById);
-usersRouter.patch('/users/me', auth, updateUser);
-usersRouter.patch('/users/me/avatar', auth, updateAvatar);
+usersRouter.get('/users', getUsers);
+usersRouter.get('/users/:id', getUserById);
+usersRouter.patch('/users/me', updateUser);
+usersRouter.patch('/users/me/avatar', updateAvatar);
 module.exports = usersRouter;
