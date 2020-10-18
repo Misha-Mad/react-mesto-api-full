d6ebc579b8a798d5a94f4575330594c5ae89a959
Mikhail Zakharov <62329450+Misha-Mad@users.noreply.github.com>
2020-10-19 02:00:04 +0300

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -9,7 +9,6 @@ module.exports = (req, res, next) => {
       .send({ message: 'Необходима авторизация' });
   }
   const token = authorization.replace('Bearer ', '');
-  console.log(token);
   let payload;
   try {
     // eslint-disable-next-line no-undef
