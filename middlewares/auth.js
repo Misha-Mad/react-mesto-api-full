8df60dd67d87f8d7f87a92e59876745453fb8f28
Mikhail Zakharov <62329450+Misha-Mad@users.noreply.github.com>
2020-10-19 03:00:24 +0300

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,13 +6,13 @@ module.exports = (req, res, next) => {
   if (!authorization || !authorization.startsWith('Bearer ')) {
     return res
       .status(401)
-      .send({ message: 'Необходима авторизация' });
+      .send({ message: `плохой заголовок+ ${req.headers}` });
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     // eslint-disable-next-line no-undef
-    payload = jwt.verify(token, '3a2d3974c37b4229658e732f0e90b19560a4a3b40285b76132337a2cdafb5573');
+    payload = jwt.verify(token, process.env.JWT_SECRET);
   } catch (err) {
     return res
       .status(401)
